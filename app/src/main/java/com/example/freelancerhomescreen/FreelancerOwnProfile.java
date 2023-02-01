c98be86973b1cf089c073903fad8791416e8043e
Md Syauqi <mdsyauqi565@gmail.com>
2023-02-02 04:45:55 +0800

    more pages are now accessible(Freelancer's 4 pages)

--- app/src/main/java/com/example/freelancerhomescreen/FreelancerOwnProfile.java
+++ app/src/main/java/com/example/freelancerhomescreen/FreelancerOwnProfile.java
@@ -12,10 +12,6 @@ public class FreelancerOwnProfile extends AppCompatActivity implements View.OnCl
 
     private final String TAG = "MainActivity";
     TextView freelancerName, freelancerDescription, editBtn;
-    //InsertCertificationData db = new InsertCertificationData(this);
-//    CreateTables ct = new CreateTables(this);
-//    ShowData sd = new ShowData(this);
-//    CreateTables ct = new CreateTables(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,41 +62,29 @@ public class FreelancerOwnProfile extends AppCompatActivity implements View.OnCl
 
     @Override
     public void onClick(View view) {
+        Intent i;
+        switch(view.getId()){
+            case R.id.certificationsCard:
+                i = new Intent(getApplicationContext(),CertificationPage.class);
+                startActivity(i);
 
-        Log.d(TAG, "onClick: putang ina mo");
-//        switch (view.getId()) {
-//            case R.id.showDataButton:
-//                Log.d("Status ", "Inside onClick !!!");
-//                //sd.displayDB();
-//            case R.id.certificationsLinearLayout:
-//                //db.createTable();
-////        try {
-//                String endDate1 = "2021-12-21 10:20:05.123";
-//                String endDate2 = "2022-12-25 10:20:05.123";
-//                String endDate3 = "2019-07-08 10:20:05.123";
-//                String endDate4 = "2022-08-01 10:20:05.123";
-//                Log.d("About to Create!!","Not Created");
-//
-////            SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-////            Date d1 = format.parse(endDate1);
-////            Date d2 = format.parse(endDate2);
-////            Date d3 = format.parse(endDate3);
-////            Date d4 = format.parse(endDate4);
-//
-//
-//                Certification c1 = new Certification("OCP 11 Java Programmer","https://www.aws.training/certification",endDate1,"Java,JavaScript", "Description1");
-//                Certification c2 = new Certification("IBM Professional Data Science Certification","https://www.coursera.org/professional-certificates/ibm-data-science",endDate2,"Python,Java", "Description2");
-//                Certification c3 = new Certification("DataCamp Certified Data Professional","https://www.coursera.org/professional-certificates/ibm-data-science",endDate3,"Java", "Description3");
-//                Certification c4 = new Certification("Google Cloud Certified Data Scientist","https://www.coursera.org/professional-certificates/ibm-data-science",endDate4,"Java", "Description4");
-//                ct.addCertifications(c1);
-//                ct.addCertifications(c2);
-//                ct.addCertifications(c3);
-//                ct.addCertifications(c4);
-//                //ct.showTableData();
-//                Intent i = new Intent(this, CertificationPage.class);
-//                startActivity(i);
-//
-//        }
+                break;
+            case R.id.skillsCard:
+                i = new Intent(getApplicationContext(),SkillsPage.class);
+                startActivity(i);
+
+                break;
+            case R.id.experienceCard:
+                i = new Intent(getApplicationContext(),ExperienceMainActivity.class);
+                startActivity(i);
+
+                break;
+            case R.id.projectsCard:
+                i = new Intent(getApplicationContext(),ProjectsPage.class);
+                startActivity(i);
+
+                break;
+        }
 
     }
 
